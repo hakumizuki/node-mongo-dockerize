f5d0caeb181044fdfe3b91dc9de6294e54b45d08
Taichi Masuyama <montanha.masu536@gmail.com>
2021-06-08 00:58:40 +0900

    OK

---
+++ README.md
@@ -0,0 +1,20 @@
+# 環境(Docker image)
+```
+Docker: 20系
+docker-compose: version '3'
+mongodb: 4.4
+node: 14-slim
+```
+
+# node + mongo
+```
+自力で node + mongodbの環境をdockerizeを使いながらdocker-composeで立ち上げられるかチャレンジのためのレポジトリです。普通にできました。見所はdockerizeのインストールのRUNの部分です。
+```
+
+
+# Author
+* Taichi Masuyama
+* montanha.masu536@gmail.com
+
+# License
+This Dockerfile is under [MIT license](https://en.wikipedia.org/wiki/MIT_License)
---
+++ src/app.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const DB_PORT = 27017;
+const HOST = '0.0.0.0';
+
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+const connURL = `mongodb://mongo:${DB_PORT}`;
+const databaseName = 'test-mongo1';
+
+MongoClient.connect(connURL, { useUnifiedTopology: true }, (error, client) => {
+    if (error) {
+        console.log(error);
+        return console.log('Database Error!!');
+    }
+
+    console.log('MongoDB successfully connected!!!');
+    const db = client.db(databaseName);
+});
+
+const isArg1Arg2 = (arg1, arg2) => {
+    return new Promise((resolve, reject) => {
+        if (arg1 === arg2) {
+            resolve('Yeah they are the same man.')
+        }
+
+        reject('Man, seriously.')
+    });
+};
+
+app.get('/', async (req, res) => {
+    res.send('Hello World! Gotcha man!');
+
+    try {
+        const result = await isArg1Arg2(1, 2);
+        console.log(result);
+    } catch (e) {
+        console.log(e);
+    }
+});
+
+app.listen(PORT, HOST, () => {
+    console.log(`App listening at http://${HOST}:${PORT}`);
+});
